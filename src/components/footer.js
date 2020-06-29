4241ccaf8d15074eb0c8f7ea3cd9f82ad776ae03
Sarthakux <ysarthak275@gmail.com>
2020-06-29 13:32:44 +0530

    deploy

---
+++ src/components/footer.js
@@ -0,0 +1,17 @@
+  
+import React from 'react';
+
+const FooterComponent = () => {
+    return (
+        <div className="footer">
+            <footer className="page-footer blue-grey darken-4">
+                <div className="container  footer-copyright">
+                    
+                    
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default FooterComponent;
